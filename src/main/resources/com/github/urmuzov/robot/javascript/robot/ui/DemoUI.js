02246599a4ea908dd7bd5b51aa001e825be030a9
Alexander A. Urmuzov <urmuzov@gmail.com>
2011-10-06 22:55:15 +0600

    Savinn and restoring user algorithms from localStorage. Refactoring follows.

--- src/main/resources/com/github/urmuzov/robot/javascript/robot/ui/DemoUI.js
+++ src/main/resources/com/github/urmuzov/robot/javascript/robot/ui/DemoUI.js
@@ -22,10 +22,20 @@ robot.ui.DemoUI = function() {
      */
     this.SELECT_NOTHING_ID = "*";
     /**
+     * @const
+     * @type string
+     */
+    this.USER_ALGORITHM_PREFIX = "user:";
+    /**
      * @private
      * @type !goog.storage.Storage
      */
     this.storage = new goog.storage.Storage(new goog.storage.mechanism.HTML5LocalStorage());
+    /**
+     * @private
+     * @type Object
+     */
+    this.userAlgorithms_ = this.getObjectFromStorage('userAlgorithms', {});
     /**
      * @private
      * @type string
@@ -68,16 +78,52 @@ robot.ui.DemoUI = function() {
      * @type !goog.ui.Slider
      */
     this.slider_ = this.createSlider('speed-slider');
+    /**
+     * @private
+     * @type Element
+     */
+    this.algorithmsSelect_ = goog.dom.getElement('algorithm-select');
+    this.updateAlgorithmSelectElement();
 
 
     this.initFieldSelectElement('field-select');
-    this.initAlgorithmSelectElement('algorithm-select');
-    this.initButtons('run-original-button', 'run-editor-button');
     this.initSliderValue('speed-value');
+
+    /**
+     * @private
+     * @type Element
+     */
+    this.runOriginalButton_ = goog.dom.getElement('run-original-button');
+    goog.events.listen(this.runOriginalButton_, goog.events.EventType.CLICK, goog.bind(this.run, this, true));
+    /**
+     * @private
+     * @type Element
+     */
+    this.runEditorButton_ = goog.dom.getElement('run-editor-button');
+    goog.events.listen(this.runEditorButton_, goog.events.EventType.CLICK, goog.bind(this.run, this, false));
+    /**
+     * @private
+     * @type Element
+     */
+    this.saveAsButton_ = goog.dom.getElement('save-as');
+    goog.events.listen(this.saveAsButton_, goog.events.EventType.CLICK, goog.bind(this.saveAs, this));
+    /**
+     * @private
+     * @type Element
+     */
+    this.saveButton_ = goog.dom.getElement('save');
+    goog.events.listen(this.saveButton_, goog.events.EventType.CLICK, goog.bind(this.save, this));
+    /**
+     * @private
+     * @type Element
+     */
+    this.removeButton_ = goog.dom.getElement('remove');
+    goog.events.listen(this.removeButton_, goog.events.EventType.CLICK, goog.bind(this.remove, this));
 
     this.reloadField();
     this.reloadAlgorithm();
     this.updateSlider();
+    this.updateButtons();
 
     new goog.debug.DivConsole(goog.dom.getElement('debug')).setCapturing(true);
 };
@@ -134,18 +180,15 @@ robot.ui.DemoUI.prototype.initFieldSelectElement = function(elementId) {
     }, this));
 };
 
-/**
- * @param {string} elementId
- */
-robot.ui.DemoUI.prototype.initAlgorithmSelectElement = function(elementId) {
-    var algorithmsSelect = goog.dom.getElement(elementId);
+robot.ui.DemoUI.prototype.updateAlgorithmSelectElement = function() {
+    goog.dom.removeChildren(this.algorithmsSelect_);
     var algorithms = robot.PredefinedAlgorithms.algorithms;
     var selectedAlgorithmId = this.getAlgorithmId();
     var selectedNothing = (selectedAlgorithmId == this.SELECT_NOTHING_ID);
     var algorithmSelected = false;
     var optionSelected = false;
     var predefinedAlgorithmsGroup = goog.dom.createDom('optgroup', {'label':'Predefined'});
-    goog.dom.appendChild(algorithmsSelect, predefinedAlgorithmsGroup);
+    goog.dom.appendChild(this.algorithmsSelect_, predefinedAlgorithmsGroup);
     for (var i in algorithms) {
         if (algorithms.hasOwnProperty(i)) {
             optionSelected = false;
@@ -163,23 +206,30 @@ robot.ui.DemoUI.prototype.initAlgorithmSelectElement = function(elementId) {
         }
     }
     var userAlgorithmsGroup = goog.dom.createDom('optgroup', {'label':'User'});
-    goog.dom.appendChild(algorithmsSelect, userAlgorithmsGroup);
-    goog.events.listen(algorithmsSelect, goog.events.EventType.CHANGE, goog.bind(function(e) {
+    var userAlgorithms = this.getUserAlgorithms();
+    for (var i in userAlgorithms) {
+        if (userAlgorithms.hasOwnProperty(i)) {
+            optionSelected = false;
+            if (selectedNothing) {
+                if (!algorithmSelected) {
+                    algorithmSelected = true;
+                    this.setAlgorithmId(i);
+                }
+            } else {
+                if (selectedAlgorithmId == this.userAlgorithmNameToId(i)) {
+                    optionSelected = true;
+                }
+            }
+            goog.dom.appendChild(userAlgorithmsGroup, goog.dom.createDom('option', {'value':this.userAlgorithmNameToId(i),'selected':optionSelected}, i));
+        }
+    }
+    goog.dom.appendChild(this.algorithmsSelect_, userAlgorithmsGroup);
+    goog.events.listen(this.algorithmsSelect_, goog.events.EventType.CHANGE, goog.bind(function(e) {
         var select = e.target;
         this.setAlgorithmId(select.options[select.selectedIndex].value);
         this.reloadAlgorithm();
+        this.updateButtons();
     }, this));
-};
-
-/**
- * @param {string} runOriginalElementId
- * @param {string} runEditorElementId
- */
-robot.ui.DemoUI.prototype.initButtons = function(runOriginalElementId, runEditorElementId) {
-    var runOriginalButton = goog.dom.getElement(runOriginalElementId);
-    goog.events.listen(runOriginalButton, goog.events.EventType.CLICK, goog.bind(this.run, this, true));
-    var runEditorButton = goog.dom.getElement(runEditorElementId);
-    goog.events.listen(runEditorButton, goog.events.EventType.CLICK, goog.bind(this.run, this, false));
 };
 
 /**
@@ -214,6 +264,12 @@ robot.ui.DemoUI.prototype.updateSlider = function () {
     goog.dom.getElement('speed-value').value = this.getAnimationTime();
 };
 
+robot.ui.DemoUI.prototype.updateButtons = function () {
+    var userAlgorithm = (this.userAlgorithmIdToName(this.getAlgorithmId()) in this.getUserAlgorithms());
+    this.removeButton_.disabled = !userAlgorithm;
+    this.saveButton_.disabled = !userAlgorithm;
+};
+
 robot.ui.DemoUI.prototype.reloadField = function () {
     goog.dispose(this.visualizer_);
     goog.dispose(this.field_);
@@ -230,11 +286,19 @@ robot.ui.DemoUI.prototype.reloadField = function () {
 };
 
 robot.ui.DemoUI.prototype.reloadAlgorithm = function () {
-    this.algorithm_ = robot.PredefinedAlgorithms.algorithms[this.getAlgorithmId()];
+    var algId = this.getAlgorithmId();
+    if (this.isUserAlgorithmId(algId)) {
+        this.algorithm_ = (/** @type function (robot.Field) */this.stringToFunction(this.getUserAlgorithms()[this.userAlgorithmIdToName(algId)]));
+    } else {
+        this.algorithm_ = robot.PredefinedAlgorithms.algorithms[algId];
+    }
     if (this.algorithm_)
-        this.editor_.getSession().setValue(this.algorithm_.toString());
+        this.setEditorValue(this.algorithm_.toString());
 };
 
+/**
+ * @param {boolean} executeOriginal
+ */
 robot.ui.DemoUI.prototype.run = function (executeOriginal) {
     if (goog.isNull(this.algorithm_)) {
         alert('Algorithm is not selected');
@@ -252,15 +316,46 @@ robot.ui.DemoUI.prototype.run = function (executeOriginal) {
         if (executeOriginal) {
             this.algorithm_(this.field_);
         } else {
-            // temporary set window['tmpField']
-            window['tmpField'] = this.field_;
-            goog.globalEval("(" + this.editor_.getSession().getValue() + ")(window['tmpField']);");
-            delete window['tmpField'];
+            var alg = this.stringToFunction(this.getEditorValue());
+            alg(this.field_);
         }
     } catch (e) {
         this.logger.severe(e.toString());
+        throw e;
     }
     this.visualizer_.play();
+};
+
+robot.ui.DemoUI.prototype.saveUserAlgorithm = function (name, algorithmText) {
+    var userAlgos = this.getUserAlgorithms();
+    userAlgos[name] = algorithmText;
+    this.setUserAlgorithms(userAlgos);
+};
+
+robot.ui.DemoUI.prototype.saveAs = function () {
+    var name = window.prompt('Enter algorithm name:');
+    if (name) {
+        var algorithmText = this.getEditorValue();
+        this.saveUserAlgorithm(name, algorithmText);
+        this.setAlgorithmId(this.userAlgorithmNameToId(name));
+        this.updateAlgorithmSelectElement();
+    }
+};
+
+robot.ui.DemoUI.prototype.save = function () {
+    var name = this.userAlgorithmIdToName(this.getAlgorithmId());
+    var algorithmText = this.getEditorValue();
+    this.saveUserAlgorithm(name, algorithmText);
+};
+
+robot.ui.DemoUI.prototype.remove = function () {
+    var userAlgos = this.getUserAlgorithms();
+    delete userAlgos[this.userAlgorithmIdToName(this.getAlgorithmId())];
+    this.setUserAlgorithms(userAlgos);
+    this.setAlgorithmId(this.SELECT_NOTHING_ID);
+    this.updateAlgorithmSelectElement();
+    this.reloadAlgorithm();
+    this.updateButtons();
 };
 
 /**
@@ -268,13 +363,7 @@ robot.ui.DemoUI.prototype.run = function (executeOriginal) {
  * @param {number} defaultValue
  */
 robot.ui.DemoUI.prototype.getNumberFromStorage = function(key, defaultValue) {
-    var num = this.storage.get(key);
-    if (goog.isDefAndNotNull(num)) {
-        return Number(num);
-    } else {
-        this.storage.set(key, defaultValue);
-        return defaultValue;
-    }
+    return Number(this.getObjectFromStorage(key, (/** @type Object */defaultValue)));
 };
 
 /**
@@ -282,9 +371,17 @@ robot.ui.DemoUI.prototype.getNumberFromStorage = function(key, defaultValue) {
  * @param {string} defaultValue
  */
 robot.ui.DemoUI.prototype.getStringFromStorage = function(key, defaultValue) {
+    return String(this.getObjectFromStorage(key, (/** @type Object */defaultValue)));
+};
+
+/**
+ * @param {string} key
+ * @param {Object} defaultValue
+ */
+robot.ui.DemoUI.prototype.getObjectFromStorage = function(key, defaultValue) {
     var str = this.storage.get(key);
     if (goog.isDefAndNotNull(str)) {
-        return String(str);
+        return str;
     } else {
         this.storage.set(key, defaultValue);
         return defaultValue;
@@ -342,4 +439,71 @@ robot.ui.DemoUI.prototype.getAlgorithmId = function () {
 robot.ui.DemoUI.prototype.setAlgorithmId = function (algorithmId) {
     this.algorithmId_ = algorithmId;
     this.setToStorage('algorithmId', this.algorithmId_);
+};
+
+/**
+ * @return {Object}
+ */
+robot.ui.DemoUI.prototype.getUserAlgorithms = function () {
+    return this.userAlgorithms_;
+};
+
+/**
+ * @param {Object} userAlgorithms
+ */
+robot.ui.DemoUI.prototype.setUserAlgorithms = function (userAlgorithms) {
+    this.userAlgorithms_ = userAlgorithms;
+    this.setToStorage('userAlgorithms', this.userAlgorithms_);
+};
+
+
+/**
+ * @return {string}
+ */
+robot.ui.DemoUI.prototype.getEditorValue = function () {
+    return this.editor_.getSession().getValue();
+};
+
+/**
+ * @param {string} editorValue
+ */
+robot.ui.DemoUI.prototype.setEditorValue = function (editorValue) {
+    this.editor_.getSession().setValue(editorValue);
+};
+
+/**
+ * @param {string} userAlgorithmId
+ * @return {string}
+ */
+robot.ui.DemoUI.prototype.userAlgorithmIdToName = function (userAlgorithmId) {
+    return goog.string.remove(userAlgorithmId, this.USER_ALGORITHM_PREFIX);
+};
+
+/**
+ * @param {string} userAlgorithmName
+ * @return {string}
+ */
+robot.ui.DemoUI.prototype.userAlgorithmNameToId = function (userAlgorithmName) {
+    return this.USER_ALGORITHM_PREFIX + userAlgorithmName;
+};
+
+
+/**
+ * @param {string} algorithmId
+ * @return {boolean}
+ */
+robot.ui.DemoUI.prototype.isUserAlgorithmId = function (algorithmId) {
+    return goog.string.startsWith(algorithmId, this.USER_ALGORITHM_PREFIX);
+};
+
+/**
+ * @param {string} str
+ * @return {Function}
+ */
+robot.ui.DemoUI.prototype.stringToFunction = function (str) {
+    var id = goog.events.getUniqueId("tmpFunction");
+    goog.globalEval("window['"+id+"'] = (" + str + ");");
+    var func = window[id];
+    delete window[id];
+    return func;
 };
