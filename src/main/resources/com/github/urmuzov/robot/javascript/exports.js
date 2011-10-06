57de340806740189734d4cdfc046c7e4fc704c73
Alexander A. Urmuzov <urmuzov@gmail.com>
2011-10-07 01:58:43 +0600

    Adding exports. Exprots don't work properly for now.

---
+++ src/main/resources/com/github/urmuzov/robot/javascript/exports.js
@@ -0,0 +1,21 @@
+goog.require('goog.math.Coordinate');
+goog.exportSymbol('goog.math.Coordinate', goog.math.Coordinate);
+
+goog.require('robot.Direction');
+goog.exportSymbol('robot.Direction', robot.Direction);
+
+goog.require('robot.figures.Figure');
+goog.exportSymbol('robot.figures.Figure', robot.figures.Figure);
+goog.exportSymbol('robot.figures.Figure.prototype.setField', robot.figures.Figure.prototype.setField);
+goog.exportSymbol('robot.figures.Figure.prototype.move', robot.figures.Figure.prototype.move);
+goog.exportSymbol('robot.figures.Figure.prototype.turnLeft', robot.figures.Figure.prototype.turnLeft);
+goog.exportSymbol('robot.figures.Figure.prototype.turnRight', robot.figures.Figure.prototype.turnRight);
+
+goog.require('robot.figures.WallE');
+goog.exportSymbol('robot.figures.WallE', robot.figures.WallE);
+
+goog.require('robot.figures.StackableFigure');
+goog.exportSymbol('robot.figures.StackableFigure', robot.figures.StackableFigure);
+
+goog.require('robot.figures.SummonerFigure');
+goog.exportSymbol('robot.figures.SummonerFigure', robot.figures.SummonerFigure);
